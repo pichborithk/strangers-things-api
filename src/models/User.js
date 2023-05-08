b2e4503787171f1e6e8a47bebfd4b7c313dbbd0d
Pichborith Kong <pichborith@gmail.com>
2023-05-07 21:24:01 -0700

    create user with crypto password

--- src/models/User.js
+++ src/models/User.js
@@ -5,8 +5,12 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true },
     authentication: {
       password: { type: String, required: true, select: false },
-      // salt: { type: String, select: false },
-      // sessionToken: { type: String, select: false },
+      salt: { type: String, select: false },
+      sessionToken: { type: String, select: false },
+    },
+    posts: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] },
+    messages: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
     },
   },
   {
