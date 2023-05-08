1cf2fdcceb7b489d44ef316c9f9cf484b2b31d11
Pichborith Kong <pichborith@gmail.com>
2023-05-07 23:04:18 -0700

    update create post to check token before post

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,24 +1,59 @@
 const mongoose = require('mongoose');
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 const createPost = async (req, res) => {
-  const { title, author } = req.body;
+  try {
+    const sessionToken = req.cookies['AUTH-STRANGERS-THINGS-API'];
+    console.log(sessionToken);
+    if (!sessionToken) {
+      res
+        .status(403)
+        .json({ success: false, message: 'Please login to make new post' });
+      return;
+    }
 
-  const post = new Post({
-    _id: new mongoose.Types.ObjectId(),
-    title,
-    author,
-  });
+    const user = await User.findOne({
+      'authentication.sessionToken': sessionToken,
+    });
+    console.log(user);
 
-  return post
-    .save()
-    .then(post => res.status(201).json({ post }))
-    .catch(error => res.status(500).json({ error }));
+    if (!user._id) {
+      res.status(403).json({ success: false, message: 'Unauthorized' });
+      return;
+    }
+
+    const { title } = req.body;
+    const post = new Post({
+      _id: new mongoose.Types.ObjectId(),
+      title,
+      author: user._id,
+    });
+    user.posts.push(post._id);
+    await post.save();
+    await user.save();
+    res.status(201).json({ post });
+    return;
+  } catch (error) {
+    res.status(500).json({ error });
+    return;
+  }
+
+  // const post = new Post({
+  //   _id: new mongoose.Types.ObjectId(),
+  //   title,
+  //   author,
+  // });
+
+  // return post
+  //   .save()
+  //   .then(post => res.status(201).json({ post }))
+  //   .catch(error => res.status(500).json({ error }));
 };
 
 const readAll = async (req, res) => {
   return Post.find()
-    .populate('author', '-__v -updatedAt -createdAt')
+    .populate('author', '-__v -updatedAt -createdAt -posts -messages')
     .populate({
       path: 'comments',
       select: '-updatedAt -createdAt -onPost',
