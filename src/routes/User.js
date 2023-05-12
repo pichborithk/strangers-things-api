ed877752607dedbb39d01458a6f0e159a0ffa420
Pichborith Kong <pichborith@gmail.com>
2023-05-12 13:36:54 -0700

    switch to use token instead of cookie

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/User');
 
-router.post('/create', controller.createUser);
+router.post('/register', controller.createUser);
 router.post('/login', controller.loginUser);
 router.get('/get', controller.readAll);
 
