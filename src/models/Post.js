84ebc4d4558ce476abfdcf593ba7df39651b09c6
Pichborith Kong <pichborith@gmail.com>
2023-05-12 21:07:32 -0700

    add readUser function and add active field for post model

--- src/models/Post.js
+++ src/models/Post.js
@@ -16,6 +16,7 @@ const PostSchema = new mongoose.Schema(
       type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
     },
     isAuthor: { type: Boolean, default: false },
+    active: { type: Boolean, default: true },
   },
   {
     timestamps: true,
